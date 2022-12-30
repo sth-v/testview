aed4a026c4bacb1b909eb182ca07d5a6791fc6ab
sth-v <aa@contextmachine.online>
2022-12-30 21:06:54 +0300

    Initial commit

---
+++ testview/testview/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  testview
+//
+//  Created by Andrew Astakhov on 29.12.22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
